7adf6818d298e2ee2e044ea330e17898de956097
Collider24 <mda_games@mail.ru>
2019-03-27 16:55:56 +1000

    Add another lever system

---
+++ Assets/Scripts/LeftRight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftRight : MonoBehaviour
+{
+    public CraneControl Cabine;
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Hand")
+        {
+            if(OVRInput.GetUp(OVRInput.Button.One))
+            {
+                Cabine.MoveUpCart(false);
+            }
+            else
+            {
+                Cabine.MoveUpCart(true);
+            }
+        }
+    }
+}
+
