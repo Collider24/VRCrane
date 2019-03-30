21c7bc50cb1d877e37f49ad3e526f60f2be80970
Klenin <Klenin@gmail.com>
2019-03-30 17:25:12 +1000

    Do Second Debug

--- Assets/Scripts/Thumbler/TumblerMechanicsThreePoints.cs
+++ Assets/Scripts/Thumbler/TumblerMechanicsThreePoints.cs
@@ -5,32 +5,17 @@ using UnityEngine;
 public class TumblerMechanicsThreePoints : MonoBehaviour
 {
     public int State = 0;
-    
+
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "Hands")
         {
             if (OVRInput.GetUp(OVRInput.Button.Four))
             {
-                State++;
-                if (State == 1)
-                {
-                    State++;
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                else if (State == 2)
-                {
-                    State++;
-                    transform.rotation = Quaternion.Euler(0, 45, 0);
-                }
-                else
-                {
-                    State = 0;
-                    transform.rotation = Quaternion.Euler(0, -45, 0);
-                }
+                State = (State + 1) % 3;
+                transform.rotation = Quaternion.Euler(transform.rotation.x, 45*State + 45, transform.rotation.z);
             }
 
         }
     }
-
 }
