2a30402fd7ee8f2e95bc24100be420ec98c73878
Klenin <Klenin@gmail.com>
2019-04-04 19:01:28 +1000

    Pre release 1

---
+++ Assets/Scripts/Hook1Grab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook1Grab : MonoBehaviour {
+
+    public bool Grabbed = false;
+    public bool InTrigger = false;
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Hook1")
+        {
+            Grabbed = true;
+            InTrigger = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hook1")
+        {
+            Grabbed = false;
+            InTrigger = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Hook2Grab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook2Grab : MonoBehaviour {
+
+    public bool Grabbed = false;
+    public bool InTrigger = false;
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Hook2")
+        {
+            Grabbed = true;
+            InTrigger = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Hook2")
+        {
+            InTrigger = false;
+            Grabbed = false;
+        }
+    }
+}
